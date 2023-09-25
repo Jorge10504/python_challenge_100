5347abff1adb2e2d07ce6742ddbfe188cda18e07
Jorge Rodriguez <58515014+Jorge10504@users.noreply.github.com>
2023-09-25 16:39:42 -0500

    Create band_name_generator.py
    This is a python program to generate a band name based on two different outputs

---
+++ band_name_generator.py
@@ -0,0 +1,11 @@
+print("Hi there, welcome to the band name generator!"
+      )
+
+city_name = input("What is the name of the city you grew up in?\n"
+                  )
+
+pet_name = input("What is the name of your first pet?\n"
+                 )
+print(
+    "The name of your band is " + city_name + " " + pet_name
+)
