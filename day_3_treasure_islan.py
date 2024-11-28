6d6ff77f16276b787ff0cb57eedfc5498cc72b56
Jorge Rodriguez <58515014+Jorge10504@users.noreply.github.com>
2024-11-28 16:36:31 -0300

    Update day_3_treasure_islan.py

--- day_3_treasure_islan.py
+++ day_3_treasure_islan.py
@@ -22,18 +22,14 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 
 print("Welcome to the Treasure Island Game!\nYour mission is to find the treasure.")
-choice = input("You are at a cross road. Where do you want to go? Type 'left' or 'right.'")
+choice = input("You are at a cross road. Where do you want to go? Type 'left' or 'right.'\n")
 
-if choice == "right":
-    print("Game Over!")
-elif choice == "left":
+if choice == "left":
     choice = input(
         "You have come to a lake. There is an island in the middle of the lake. "
         "Type 'wait' to wait for a boat or 'swim' to swim to the island.\n"
     )
-    if choice == "wait":
-        print("Game Over!")
-    elif choice == "swim":
+    if choice == "swim":
         choice = input(
             "You arrive at the island unharmed. "
             "There is a house with three doors. One is red, one yellow and one blue. "
@@ -41,5 +37,13 @@ elif choice == "left":
         )
         if choice == "red":
             print("Congratulations, you have found the treasure!")
+        elif choice == "yellow":
+            print("You enter a room full of fire, you lost!")
+        elif choice == "blue":
+            print("You enter a room full of beasts, you lost!")
         else:
-            print("Incorrect! Game Over.")
+            print("You picked a door that doesn't exist, you lost!")
+    else:
+        print("You got attacked by a bear! Game Over.")
+else:
+    print("Game Over!")
