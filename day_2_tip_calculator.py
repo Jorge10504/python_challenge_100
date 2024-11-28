29260b2e870cf787bb035dfaa8731b3da1243aa4
Jorge Rodriguez <58515014+Jorge10504@users.noreply.github.com>
2024-11-28 11:41:19 -0300

    Update day_2_tip_calculator.py

--- day_2_tip_calculator.py
+++ day_2_tip_calculator.py
@@ -1,13 +1,15 @@
-print("Welcome to the bill splitting calculator")
-bill = float(input("What is the total bill?\n"))
-tip = int(input("What is your desired tip percentage?\n"))
-people_paying = float(input("How many people are splitting the bill\n"))
+print("Welcome to the tip calculator!")
+total_bill = float(input("What is the bill amount?\n"))
+tip_choice = input("Would you like to add a tip? Y/N \n")
 
-tip_amount = (tip / 100) * bill
-
-bill_total = bill + tip_amount
-
-split_total = round(bill_total / people_paying, 2)
-split_total = "{:.2f}".format(split_total)
-
-print(f"Each person is paying {split_total}")
+if tip_choice == "Y":
+    tip_percentage = int(input("How much would you like to tip? 10%, 12%, 15%\n"))
+    tip_amount = total_bill * tip_percentage / 100
+    number_of_people = int(input("How many people are splitting the bill?\n"))
+    final_bill = round(total_bill + tip_amount, 2)
+    individual_bill = round(final_bill / number_of_people, 2)
+    print(f"The total bill is: ${final_bill}. Each person must pay: ${individual_bill}")
+elif tip_choice == "N":
+    number_of_people = int(input("How many people are splitting the bill?\n"))
+    individual_bill = round(total_bill / number_of_people, 2)
+    print(f"Each person must pay: ${individual_bill}")
